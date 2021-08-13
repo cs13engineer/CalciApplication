b804c84bffd88d7f82c38b6a7116bc19f4813b3a
cs13engineer <75882819+cs13engineer@users.noreply.github.com>
2021-08-13 13:55:47 +0530

    Its a Main template of App!

---
+++ calci.go
@@ -0,0 +1,43 @@
+package calci
+
+//will add integers
+func addInt(i int, j int)int{
+	return(i+j)
+}
+
+//will add float32
+func addFloat(a float32, b float32)float32{
+	return(a+b)
+}
+
+//will subtract integers
+func subInt(c int, d int)int{
+	return(c-d)
+}
+
+//will subtract float32
+func subFloat(e float32, f float32)float32{
+	return(e-f)
+}
+
+//will multiply integers
+func subMul(g int, h int)int{
+	return(g*h)
+}
+
+//will multiply float32
+func subMul(k float32, l float32)float32{
+	return(k*l)
+}
+
+//will divide integers
+func subDiv(m int, n int)int{
+	return(m/n)
+}
+
+//will divide the Float32 type
+func subDiv(o float32, p float32)float32{
+	return(o/p)
+}
+
+
